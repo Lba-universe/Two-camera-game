04e185047d97e7ba46b48c3e75be4eec7b8eca2f
liad <liadbenmoshe10@gmail.com>
2020-11-02 00:06:52 +0200

    first

---
+++ Assets/move.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+                 {
+                         Vector3 position = this.transform.position;
+                         position.x-=(float)0.1;
+                         this.transform.position = position;
+                 }
+                 if (Input.GetKey(KeyCode.RightArrow))
+                 {
+                         Vector3 position = this.transform.position;
+                         position.x+=(float)0.1;
+                         this.transform.position = position;
+                 }
+                 if (Input.GetKey(KeyCode.UpArrow))
+                 {
+                         Vector3 position = this.transform.position;
+                         position.y+=(float)0.1;
+                         this.transform.position = position;
+                 }
+                 if (Input.GetKey(KeyCode.DownArrow))
+                 {
+                         Vector3 position = this.transform.position;
+                         position.y-=(float)0.1;
+                         this.transform.position = position;
+                 }
+    }
+    
+}
